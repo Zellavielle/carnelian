4e40e38e021cee06faed22f9cf90db15ec2afeab
Vick L. <zellavielle@gmail.com>
2024-09-07 22:18:51 -0300

    Update redirect.js

--- JS/redirect.js
+++ JS/redirect.js
@@ -1,15 +1,15 @@
+// Entoura Themes
 document.addEventListener('click', function(event) {
-    // Verifica si el elemento clicado es un enlace (etiqueta <a>)
-    if (event.target.tagName === 'A') {
-        const href = event.target.getAttribute('href');
-        
-        // Comprueba si el href termina en "/u" seguido de un número (usando expresión regular)
+    const linkElement = event.target.closest('a');
+    if (linkElement) {
+        const href = linkElement.getAttribute('href');
         if (href && /\/u\d+$/.test(href)) {
-            // Evita la redirección normal del enlace
             event.preventDefault();
-            
-            // Redirige a la página de inicio de sesión
-            window.location.href = '/login?redirect=%2Fu7'; // Ajusta esto a la URL de tu página de inicio de sesión
+            window.location.href = '/login';
         }
     }
+});
+
+window.addEventListener('error', function(event) {
+    console.error("Error caught:", event.message);
 });
